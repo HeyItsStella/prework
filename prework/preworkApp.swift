1768d81059ced396160faa3ed14aa8608ad9c12c
stella <yinglan165@gmail.com>
2023-08-25 01:13:15 -0500

    Initial Commit

---
+++ prework/preworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  preworkApp.swift
+//  prework
+//
+//  Created by Stella_X on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct preworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
